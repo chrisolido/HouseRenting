38b6e13b6c4d45d76d32801447683c78e53a5042
Halbator <kevin.barre@epitech.eu>
2017-11-30 09:26:29 +0800

    first commit

---
+++ project/app/models.py
@@ -0,0 +1,77 @@
+from mongoengine import Document, EmbeddedDocument, fields
+from users.models import User
+
+
+class Author(Document):
+    name = fields.StringField()
+
+
+class Book(Document):
+    name = fields.StringField()
+    author = fields.ReferenceField(Author, dbref=True)
+
+
+class ToolInput(EmbeddedDocument):
+    id = fields.StringField(required=True)
+    type = fields.ListField(fields.DynamicField(null=True))
+    label = fields.StringField(required=True, null=True)
+    description = fields.StringField(required=False, null=True)
+    default = fields.DynamicField(required=False)
+    inputBinding = fields.DynamicField(required=True)
+    required = fields.BooleanField(required=False, default=True)
+
+
+class ToolOutput(EmbeddedDocument):
+    id = fields.StringField(required=True)
+    type = fields.ListField(fields.DynamicField(null=True))
+    label = fields.StringField(required=False)
+    default = fields.DynamicField(required=False, null=True)
+    description = fields.StringField(required=False)
+    outputBinding = fields.DynamicField(required=False)
+    required = fields.BooleanField(required=False, default=True)
+
+
+class Tool(Document):
+    id = fields.StringField(required=True, primary_key=True)
+    # 'class' is a reserved word in python, so to get a field called "class", we use the following trick with vars():
+    vars()['class'] = fields.StringField(verbose_name="class", required=True)
+    label = fields.StringField(required=True)
+    description = fields.StringField(required=True, null=True)
+    owner = fields.ListField(fields.StringField())
+    contributor = fields.ListField(fields.StringField())
+    inputs = fields.EmbeddedDocumentListField(ToolInput)
+    outputs = fields.EmbeddedDocumentListField(ToolOutput)
+    baseCommand = fields.DynamicField(required=True)
+    arguments = fields.DynamicField(required=True)
+    requirements = fields.DynamicField(required=True, null=True)
+    hints = fields.DynamicField(required=False, null=True)
+    cwlVersion = fields.StringField(required=False, null=True, choices=['cwl:draft-2'])
+    stdin = fields.StringField(required=False, null=True)
+    stdout = fields.StringField(required=False, null=True)
+    successCodes = fields.ListField(fields.IntField(), required=False)
+    temporaryFailCodes = fields.ListField(fields.IntField(), required=False)
+    permanentFailCodes = fields.ListField(fields.IntField(), required=False)
+
+
+class Address(EmbeddedDocument):
+    country = fields.StringField(required=True)
+    city = fields.StringField(required=True)
+    road = fields.StringField(required=True)
+    province = fields.StringField(required=True)
+    district = fields.StringField(required=True)
+    floor = fields.IntField(required=True)
+
+
+class House(Document):
+    contact = fields.ReferenceField(User, dbref=True)
+    title = fields.StringField(required=True, verbose_name="Title")
+    price = fields.FloatField(required=True, verbose_name="Price")
+    address = fields.EmbeddedDocumentField(Address)
+    from_date = fields.DateTimeField(verbose_name="Available  date")
+    to_date = fields.DateTimeField(verbose_name="Available date")
+    size = fields.IntField(verbose_name="room size")
+    room = fields.DecimalField(min_value=0, max_value=10, default=0, precision=0, verbose_name="Number of room")
+    information = fields.DynamicField(verbose_name="Description")
+    vars()['type'] = fields.StringField(required=True, choices=['Group', 'Alone'])
+    # pictures = fields.ListField(fields.ImageField(), required=False)
+
---
+++ project/users/views.py
@@ -0,0 +1,44 @@
+from rest_framework import views, mixins, permissions, exceptions
+from rest_framework.response import Response
+from rest_framework_mongoengine import viewsets
+from rest_framework import parsers, renderers
+from django.middleware.csrf import get_token
+from users.serializers import *
+from users.models import *
+from users.authentication import TokenAuthentication
+
+
+class UserViewSet(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  viewsets.GenericViewSet):
+    """
+    Read-only User endpoint
+    """
+    permission_classes = (permissions.IsAuthenticated, )  # IsAdminUser?
+    authentication_classes = (TokenAuthentication, )
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.all()
+
+
+class ObtainAuthToken(views.APIView):
+    throttle_classes = ()
+    permission_classes = ()
+    authentication_classes = (TokenAuthentication, )
+    # parser_classes = (parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser,)
+    # renderer_classes = (renderers.JSONRenderer,)
+    serializer_class = AuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+    def get(self, request, *arg, **kwargs):
+        return Response({"csrfmiddlewaretoken": get_token(request)})
+
+
+obtain_auth_token = ObtainAuthToken.as_view()
